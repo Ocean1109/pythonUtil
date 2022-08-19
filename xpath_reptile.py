350fac1260db06b4a0e6f95d17a2bba92d5cac8d
Ocean <15011099680@163.com>
2022-08-19 11:55:50 +0800

    update

---
+++ xpath_reptile.py
@@ -0,0 +1,49 @@
+import urllib.request
+from lxml import etree
+
+
+def create_request(page):
+    if (page == 1):
+        url = 'https://sc.chinaz.com/tupian/qinglvtupian.html'
+    else:
+        url = 'https://sc.chinaz.com/tupian/qinglvtupian_' + str(page) + '.html'
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
+    }
+
+    request = urllib.request.Request(url=url, headers=headers)
+    return request
+
+
+def get_content(request):
+    response = urllib.request.urlopen(request)
+    content = response.read().decode('utf-8')
+    return content
+
+
+def down_load(content):
+    tree = etree.HTML(content)
+
+    name_list = tree.xpath('//div[@class="tupian-list com-img-txt-list"]//img/@alt')
+    src_list = tree.xpath('///div[@class="tupian-list com-img-txt-list"]//img/@data-original')
+
+    for i in range(len(name_list)):
+        name = name_list[i]
+        src = src_list[i]
+        url = 'https:' + src
+
+        urllib.request.urlretrieve(url=url, filename='./imageDemo/' + name + '.jpg')
+
+
+if __name__ == '__main__':
+    start_page = int(input('请输入起始页码'))
+    end_page = int(input('请输入结束页码'))
+
+    for page in range(start_page, end_page + 1):
+        # (1) 请求对象的定制
+        request = create_request(page)
+        # （2）获取网页的源码
+        content = get_content(request)
+        # （3）下载
+        down_load(content)
